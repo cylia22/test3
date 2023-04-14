c9427322e5b068573384902226cdbbe613cf4b94
zSyLL <77125297+cylia22@users.noreply.github.com>
2023-04-14 10:14:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const handrail = document.querySelector('#handrail');
-const ledStrip = document.querySelector('#led-strip');
-const ledOptions = document.querySelector('#ledOptions');
+// get the LED option radio buttons
+let ledYes = document.getElementById('ledYes');
+let ledNo = document.getElementById('ledNo');
 
-const
+// get the LED options fieldset
+let ledOptions = document.getElementById('ledOptions');
+
+// add event listeners to the LED option radio buttons
+ledYes.addEventListener('change', function() {
+  // show the LED options fieldset when "Yes" is selected
+  ledOptions.style.display = "block";
+});
+
+ledNo.addEventListener('change', function() {
+  // hide the LED options fieldset when "No" is selected
+  ledOptions.style.display = "none";
+});
+
